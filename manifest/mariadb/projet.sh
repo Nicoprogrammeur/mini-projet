c5e34112c1646267fe925b7bf519d279c36adc8f
Damien73290 <damien.shirtliffe@master-stic.fr>
2022-04-22 14:01:21 +0000

    manifest/mariadb/projet.sql

---
+++ manifest/mariadb/projet.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Secure root account
+mysql -e "UPDATE mysql.user SET Password = PASSWORD('passwordDbUniv01') WHERE User = 'root'"
+# Create database
+mysql -e "CREATE DATABASE IF NOT EXISTS projet"
+# Create application account
+mysql -e "GRANT ALL ON *.* TO 'websiteaccount'@'localhost' IDENTIFIED BY 'passwordDbUniv01' WITH GRANT OPTION;"
+# Make our changes take effect
+mysql -e "FLUSH PRIVILEGES"
--- manifest/mariadb/projet.sql
+++ manifest/mariadb/projet.sql
@@ -1,1 +1,33 @@
-CREATE DATABASE IF NOT EXISTS `webiptables` ;
+CREATE DATABASE IF NOT EXISTS `projet` ;
+
+USE `projet`;
+   CREATE TABLE IF NOT EXISTS `projet`.`localisation` ( 
+  `idloc` int(11) DEFAULT NULL,
+  `vdepart` varchar(50) DEFAULT NULL,
+  `pdepart` varchar(50) DEFAULT NULL,
+  `varrive` varchar(50) DEFAULT NULL,
+  `parrive` varchar(50) DEFAULT NULL,
+ 
+  `prix` float(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `projet`.`localisation` ( `idloc`,`vdepart`,`pdepart`,`varrive`,`parrive`) VALUES
+	(1,`Lyon`,`France`,`Venise`,`Italie`);
+    (2,`Paris`,`France`,`Venise`,`Italie`);
+    (3,`Paris`,`France`,`Los-Angeles`,`USA`);
+
+CREATE TABLE IF NOT EXISTS `projet`.`voyage` (
+  `idvoyage` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+   CONSTRAINT `FK_localisation` FOREIGN KEY (`loc_id`) REFERENCES `projet`.`localisation`(`idloc`)
+  `loc_id` int(11) DEFAULT NULL,
+  `nomvoyage` varchar(50) DEFAULT NULL,
+  `hdepart` varchar(6) DEFAULT NULL,
+  `harrive` varchar(6) DEFAULT NULL,
+  `durre` varchar(6) DEFAULT NULL,
+  `prix` float(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `projet`.`voyage` ( `idvoyage`,`idloc`,`nomvoyage`,`hdepart`,`harrive`,`durre`,`prix`) VALUES
+	(1,`Escale Lyon - Venise`,`8h30`,`9h50`,'1h20',60);
+    (2,`Escale Paris -  Venise`,`10h30`,`12h10`,`1h40`,120);
+    (3,`Escale Paris - LA`,`12h05`,`13h15`,`11h10`,600);
+
+ 
